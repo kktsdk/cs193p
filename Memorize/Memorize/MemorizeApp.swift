78bf5b6a59e42bb7383cb16ab834667972907cee
kong <kitsada@odds.team>
2023-12-10 23:28:56 +0700

    Create new project

---
+++ Memorize/Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by kktsdk on 10/12/2566 BE.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
